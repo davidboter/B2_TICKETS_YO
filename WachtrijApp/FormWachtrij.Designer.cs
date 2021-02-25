eda41d38c68ec9b01876fcf1e37f8c4ed95a4682
davidboter <d278076@edu.rocwb.nl>
2021-02-25 10:48:34 +0100

    fix #6
    Changed Title to "debug hero"

--- WachtrijApp/FormWachtrij.Designer.cs
+++ WachtrijApp/FormWachtrij.Designer.cs
@@ -45,9 +45,10 @@
             this.labelTitel.Font = new System.Drawing.Font("Cambria", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelTitel.Location = new System.Drawing.Point(26, 18);
             this.labelTitel.Name = "labelTitel";
-            this.labelTitel.Size = new System.Drawing.Size(186, 32);
+            this.labelTitel.Size = new System.Drawing.Size(152, 32);
             this.labelTitel.TabIndex = 0;
-            this.labelTitel.Text = "Bugs of Horror";
+            this.labelTitel.Text = "Debug Hero";
+            this.labelTitel.Click += new System.EventHandler(this.labelTitel_Click);
             // 
             // labelWachttijd
             // 
